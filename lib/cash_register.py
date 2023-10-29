93b976d4fd9114edbe63e14f5613359b6b94c775
Rachel Hamby <tnthreat33@gmail.com>
2023-10-29 17:27:01 -0400

    done

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
+
+    def add_item(self, item, price, quantity=1):
+        self.total += price * quantity
+        for _ in range(quantity):
+            self.items.append(item)
+        self.previous_transactions.append(
+            {"item": item, "quantity": quantity, "price": price}
+        )
